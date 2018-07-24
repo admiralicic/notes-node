dbce29b4a6ac3ff08691f28c1f7a06d363344803
Admir Alicic <admir.alicic@gmail.com>
2018-07-24 22:42:25 +0200

    Add lodash

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
+var filteredArray = _.uniq(['Admir', 'Admir', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString('Admir'));
 // var user = os.userInfo();
 
 // fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
@@ -12,8 +18,8 @@ const notes = require('./notes');
 //     }
 // });
 
-let res = notes.addNote();
-console.log(res);
+// let res = notes.addNote();
+// console.log(res);
 
-let added = notes.add(5, 10);
-console.log(added);
+// let added = notes.add(5, 10);
+// console.log(added);
