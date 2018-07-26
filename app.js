42bf6963edb92019483cfe4572fdd9b28f9986ea
Admir Alicic <admir_alicic@yahoo.com>
2018-07-26 07:45:24 +0200

    Store to file

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ console.log(command);
 console.log(argv);
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
+    let note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log('Note added');
+        console.log('-------------');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+        console.log('-------------');
+    } else {
+        console.log('Note title already in use!');
+    }
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
--- notes.js
+++ notes.js
@@ -1,7 +1,39 @@
 console.log('Starting notes!');
 
+const fs = require('fs');
+
+const fetchNotes = () => {
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (error) {
+        notes = [];
+    }
+
+    return notes;
+}
+
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
 const addNote = (title, body) => {
-    console.log('Adding', title, body);
+    let notes = fetchNotes();
+    let note = {
+        title,
+        body
+    }
+
+    
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+
 }
 
 const getAll = () => {
